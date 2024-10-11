1d280026d3c46db7559a5a072cc305af76505092
Fauzan <fauzanahsanudin@gmail.com>
2024-10-11 11:32:29 +0700

    After add some js

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById("CV").addEventListener("click", function() {
+  window.location.href = "https://drive.google.com/file/d/19f3bSYFbZTvG3eKWngkDdv9s8fWTQdK5/view?usp=sharing"; 
+});
