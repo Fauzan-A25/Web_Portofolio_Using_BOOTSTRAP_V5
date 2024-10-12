0969c4e09e2baf3a8847ee53c34e3bdcab3d4e71
Fauzan <fauzanahsanudin@gmail.com>
2024-10-12 11:45:55 +0700

    already launch v1

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 document.getElementById("CV").addEventListener("click", function() {
   window.location.href = "https://drive.google.com/file/d/19f3bSYFbZTvG3eKWngkDdv9s8fWTQdK5/view?usp=sharing"; 
 });
+
+document.addEventListener('contextmenu', event => event.preventDefault());
+  document.addEventListener('keydown', function(e) {
+    // Disable F12, Ctrl+Shift+I, Ctrl+Shift+J, Ctrl+U
+    if (e.keyCode == 123 || (e.ctrlKey && e.shiftKey && (e.keyCode == 'I'.charCodeAt(0) || e.keyCode == 'J'.charCodeAt(0))) || (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0))) {
+      e.preventDefault();
+    }
+  });
