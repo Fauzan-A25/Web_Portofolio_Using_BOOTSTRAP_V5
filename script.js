ff48d51f4a6ee3a7bfc4fa073bb5be41fd6df055
Fauzan <fauzanahsanudin@gmail.com>
2024-10-13 20:12:34 +0700

    readdyyy

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ document.getElementById("CV").addEventListener("click", function() {
   window.location.href = "https://drive.google.com/file/d/19f3bSYFbZTvG3eKWngkDdv9s8fWTQdK5/view?usp=sharing"; 
 });
 
-document.addEventListener('contextmenu', event => event.preventDefault());
-  document.addEventListener('keydown', function(e) {
-    if (e.keyCode == 123 || (e.ctrlKey && e.shiftKey && (e.keyCode == 'I'.charCodeAt(0) || e.keyCode == 'J'.charCodeAt(0))) || (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0))) {
-      e.preventDefault();
-    }
-  });
+// document.addEventListener('contextmenu', event => event.preventDefault());
+//   document.addEventListener('keydown', function(e) {
+//     if (e.keyCode == 123 || (e.ctrlKey && e.shiftKey && (e.keyCode == 'I'.charCodeAt(0) || e.keyCode == 'J'.charCodeAt(0))) || (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0))) {
+//       e.preventDefault();
+//     }
+//   });
 
   window.addEventListener('scroll', function() {
     var sections = document.querySelectorAll('section');
@@ -18,7 +18,6 @@ document.addEventListener('contextmenu', event => event.preventDefault());
         var sectionHeight = section.offsetHeight;
         var scrollPos = window.pageYOffset;
 
-        // Jika scroll melewati bagian ini
         if (scrollPos >= sectionTop - sectionHeight / 3 && scrollPos < sectionTop + sectionHeight) {
             navLinks.forEach(function(link) {
                 link.classList.remove('active');
